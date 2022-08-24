97ade59348e0c37788c415a936309a1cd770f0eb
CB Du Rietz <cbdurietz@gmail.com>
2022-08-24 10:45:01 +0200

    Add project files.

---
+++ EditorConfigLab/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Nisse {
+
+    public Nisse() {
+        DoSomething(1);
+    }
+
+    public bool Enabled { get; set; } = false;
+
+    public void DoSomething(int x) {
+
+    }
+
+}
